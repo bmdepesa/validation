5557e9d951e5dd5d3bac59aba1ee044dfd92f241
Brandon DePesa <brandon@rancher.com>
2019-04-21 11:12:19 -0700

    test pipeline

---
+++ tests/v3_api/scripts/pipeline/testscript.groovy
@@ -0,0 +1,27 @@
+#!groovy
+
+rootPath = "/src/rancher-validation/"
+containerPrefix = "${JOB_NAME}${BUILD_NUMBER}"
+rancherConfig = "rancher_env.config"
+imageName = "rancher-validation-${JOB_NAME}${env.BUILD_NUMBER}"
+testsDir = "tests/v3_api/"
+paramsConfigFile = "params_env.config"
+
+containerName = containerPrefix + "-validation"
+
+node {
+  try {
+    wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm', 'defaultFg': 2, 'defaultBg':1]) {
+      deleteDir()
+
+      stage('Precheck') { 
+        def vars = [CLUSTER_TYPES, EXTRA_VARS, K8S_UPGRADE_VERSIONS] 
+        loadEnvironmentVariables [variables: []]
+        sh "env"
+      }
+    }
+  } catch (Exception err) {
+      //
+  } finally {
+      // finally
+  }
