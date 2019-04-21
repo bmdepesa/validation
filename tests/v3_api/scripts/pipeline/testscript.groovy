3ef58f1c2c01a7aeb21d79691917a23dcd1e0a21
Brandon DePesa <brandon@rancher.com>
2019-04-21 11:43:21 -0700

    test

--- tests/v3_api/scripts/pipeline/testscript.groovy
+++ tests/v3_api/scripts/pipeline/testscript.groovy
@@ -17,7 +17,10 @@ node {
       stage('Precheck') { 
         def params = [variables: [CLUSTER_TYPES, EXTRA_VARS, K8S_UPGRADE_VERSIONS]] 
         loadEnvironmentVariables this, params
-        println("${env.ver3}")
+        println("test: ${env.ver3}")
+        println("test2: ${ver3}")
+        println("test3: " + var1)
+        println("test4: " + $type1)
       }
     }
   } catch (Exception err) {
